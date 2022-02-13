86d6c4bab11164ea2d080d6bbef62d5f50f92e19
Kyle Buchanan <kylebuch8@gmail.com>
2022-02-13 13:17:34 -0500

    removing out-tsc

--- out-tsc/src/bike-factory.js
+++
@@ -1,3 +0,0 @@
-import { BikeFactory } from './BikeFactory.js';
-customElements.define('bike-factory', BikeFactory);
-//# sourceMappingURL=bike-factory.js.map
--- out-tsc/src/machines/counter.js
+++
@@ -1,7 +0,0 @@
-export const counterInterval = (send, receieve) => {
-    const intervalId = setInterval(() => {
-        send({ type: 'TICK' });
-    }, 1000);
-    return () => { clearInterval(intervalId); };
-};
-//# sourceMappingURL=counter.js.map
